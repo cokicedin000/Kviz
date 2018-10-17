efff1f18e06e7ddd959ce816b60f4d48c1699e94
Edin Cokic <cokicedin000@gmail.com>
2018-10-17 16:41:17 +0200

    Pocetna verzija

---
+++ src/Kviz.java
@@ -0,0 +1,30 @@
+
+public class Kviz {
+    
+    public static void main(String[] args) {
+	
+    } 
+    
+    public double sabiranje (double a, double b) {
+	return 0;
+    }
+    
+    public double oduzimanje (double a, double b) {
+	return 0;
+    }
+    
+    public double mnozenje (double a, double b) {
+	return 0;
+    }
+    
+    public double dijeljenje (double a, double b) {
+	return 0;
+    }
+    public double korjenovanje (double a) {
+	return 0;
+    }
+    
+    public double kvadriranje (double a) {
+	return 0;
+    }
+}
